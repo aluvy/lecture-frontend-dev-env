cbbacdd66326d2efc8783732133ea26624614f5a
miok <aluvy_@naver.com>
2024-01-16 16:14:38 +0900

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import MainController from "./controllers/MainController.js";
+import "./app.scss";
+
+document.addEventListener("DOMContentLoaded", () => {
+  new MainController();
+});
+
+if (module.hot) {
+  console.log('핫 모듈 켜짐');
+
+  // module.hot.accept("./result", async ()=>{
+  //   console.log("result 모듈 변경 됨");
+  //   resultEl.innerHTML = await result.render();
+  // });
+}
+
+console.log(MODE);
